b0adaffdf63deb7638e4534c5d1d0d2bf081ff0d
dnabil <daffanabilhakim@gmail.com>
2023-10-09 11:57:57 +0700

    feat(user): add nama,jenjang,etc fields

--- db/entity/user.go
+++ db/entity/user.go
@@ -7,6 +7,14 @@ type User struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 
-	NIM      string `gorm:"type:VARCHAR(20);UNIQUE"`
+	NIM      string `gorm:"type:VARCHAR(20);UNIQUE NOT NULL"`
 	Password string `gorm:"type:VARCHAR(255);NOT NULL"`
+	Nama         string `gorm:"type:VARCHAR(255)"`
+	Jenjang      string `gorm:"type:VARCHAR(255)"`
+	Fakultas     string `gorm:"type:VARCHAR(255)"`
+	Jurusan      string `gorm:"type:VARCHAR(255)"`
+	ProgramStudi string `gorm:"type:VARCHAR(255)"`
+	Seleksi      string `gorm:"type:VARCHAR(255)"`
+	NomorUjian   string `gorm:"type:VARCHAR(255)"`
+	FotoProfil   string `gorm:"type:VARCHAR(255)"`
 }
